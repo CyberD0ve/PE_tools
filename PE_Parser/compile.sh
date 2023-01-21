7b6d62c48e913ac69b97de2bd1897405e2f7726c
CyberD0ve <peacendove@proton.me>
2023-01-21 18:11:07 +0100

    [Misc.] Move binary output in bin folder after compiling

--- PE_Parser/compile.sh
+++ PE_Parser/compile.sh
@@ -5,7 +5,7 @@ x86_64-w64-mingw32-gcc PEParser.c -c -o ./bin/PEParser.o
 x86_64-w64-mingw32-gcc main.c -c -o ./bin/main.o
 
 # Compile Binary Output
-x86_64-w64-mingw32-gcc ./bin/PEParser.o ./bin/main.o -o PEParser
+x86_64-w64-mingw32-gcc ./bin/PEParser.o ./bin/main.o -o ./bin/PEParser
 
 # Cleaning
 rm ./bin/*.o
